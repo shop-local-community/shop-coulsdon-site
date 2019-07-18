5db2d006a8959cef629e056433838059d82a13e5
Kristian Matthews <kristian.matthews@me.com>
2019-07-18 11:35:31 +0000

    Updates 25

---
+++ precache-manifest.cea2a5b2cfcab016f30a2efa45557466.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "11b0a8f1a1aa405e1b632af4c3795711",
+    "url": "/fonts/toolkit-entypo.11b0a8f1.eot"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "2426efb8cc44e7d2b30a",
+    "url": "/js/chunk-vendors.c22c82d2.js"
+  },
+  {
+    "revision": "8b01f5a176d11e839cf5",
+    "url": "/js/app.7b85e4e3.js"
+  },
+  {
+    "revision": "45625cbea6095a2c1231",
+    "url": "/js/about.88367ec4.js"
+  },
+  {
+    "revision": "5896498dfa6cac66236a6b97e16012b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f72b11450b8249efb6f5f089de807bd",
+    "url": "/img/logo-white.6f72b114.png"
+  },
+  {
+    "revision": "9d7b7755052a4fd0f4552b0c719c1187",
+    "url": "/img/boxed-water-is-better-1464058-unsplash.9d7b7755.jpg"
+  },
+  {
+    "revision": "9a15846e166ac591ac41f0811eb799f6",
+    "url": "/fonts/toolkit-entypo.9a15846e.woff"
+  },
+  {
+    "revision": "72e22399f55b248b952e1710704b4329",
+    "url": "/fonts/toolkit-entypo.72e22399.woff2"
+  },
+  {
+    "revision": "3c0e8c3b983f2a3011d6d3203f475ca2",
+    "url": "/fonts/toolkit-entypo.3c0e8c3b.ttf"
+  },
+  {
+    "revision": "8b01f5a176d11e839cf5",
+    "url": "/css/app.99193d1a.css"
+  }
+];
