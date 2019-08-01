c016f93eb684b5a5492413448480c7c0951a2080
Kristian Matthews <kristian.matthews@me.com>
2019-08-01 10:40:59 +0100

    Create Map.vue

---
+++ src/views/Map.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="block block-fill-height block-map">
+    <MenuButton/>
+
+    <LocalMap class="block-background" v-bind:businesses="businesses" v-bind:carParks="carParks" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import MenuButton from '@/components/MenuButton.vue'
+import LocalMap from '@/components/LocalMap.vue'
+import businessesData from '@/assets/businesses.json'
+import carParksData from '@/assets/car-parks.json'
+
+export default {
+  name: 'map',
+  components: {
+    MenuButton,
+    LocalMap
+  },
+  data () {
+    return {
+      businesses: businessesData,
+      carParks: carParksData
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import "~@shop-local/shop-local-theme/scss/variables";
+
+.block-map {
+  height: 100vh;
+}
+</style>
