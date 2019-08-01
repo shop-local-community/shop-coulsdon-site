0769c3ae0cb02aeec5507e2266ca52a069fa1b5e
Kristian Matthews <kristian.matthews@me.com>
2019-08-01 10:06:55 +0000

    Updates 31

--- precache-manifest.66babfa7ed42de0e2db7aa77b9d309e5.js
+++ precache-manifest.66babfa7ed42de0e2db7aa77b9d309e5.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = [
   {
-    "revision": "9a15846e166ac591ac41f0811eb799f6",
-    "url": "/fonts/toolkit-entypo.9a15846e.woff"
+    "revision": "11b0a8f1a1aa405e1b632af4c3795711",
+    "url": "/fonts/toolkit-entypo.11b0a8f1.eot"
   },
   {
     "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
     "url": "/robots.txt"
   },
   {
-    "revision": "11b0a8f1a1aa405e1b632af4c3795711",
-    "url": "/fonts/toolkit-entypo.11b0a8f1.eot"
+    "revision": "3c0e8c3b983f2a3011d6d3203f475ca2",
+    "url": "/fonts/toolkit-entypo.3c0e8c3b.ttf"
   },
   {
-    "revision": "50a5e7415cf1afc724ee",
-    "url": "/js/app.cfbcdf8e.js"
+    "revision": "d3f32bd555fc0fcd6b6e",
+    "url": "/js/app.c0c34fff.js"
   },
   {
     "revision": "7bd1e1ed12b352271726",
@@ -28,7 +28,7 @@ self.__precacheManifest = [
     "url": "/js/about.e91605f6.js"
   },
   {
-    "revision": "fb8bdb600b63894b91741055f8255ed5",
+    "revision": "e35f53838786e9689ae8db1ec3eb6617",
     "url": "/index.html"
   },
   {
@@ -36,23 +36,27 @@ self.__precacheManifest = [
     "url": "/img/logo-white.6f72b114.png"
   },
   {
+    "revision": "73881a56de8b0819055a6018c0c61dda",
+    "url": "/img/glyph-selected@3x.73881a56.png"
+  },
+  {
     "revision": "9d7b7755052a4fd0f4552b0c719c1187",
     "url": "/img/boxed-water-is-better-1464058-unsplash.9d7b7755.jpg"
+  },
+  {
+    "revision": "9a15846e166ac591ac41f0811eb799f6",
+    "url": "/fonts/toolkit-entypo.9a15846e.woff"
   },
   {
     "revision": "72e22399f55b248b952e1710704b4329",
     "url": "/fonts/toolkit-entypo.72e22399.woff2"
   },
   {
-    "revision": "3c0e8c3b983f2a3011d6d3203f475ca2",
-    "url": "/fonts/toolkit-entypo.3c0e8c3b.ttf"
-  },
-  {
     "revision": "ced2875c9660a20e8230",
     "url": "/css/map.3d42803a.css"
   },
   {
-    "revision": "50a5e7415cf1afc724ee",
-    "url": "/css/app.6b3f0439.css"
+    "revision": "d3f32bd555fc0fcd6b6e",
+    "url": "/css/app.b4e1ebfb.css"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.12d8548143236c48ec731d5b7e60ff30.js"
+  "/precache-manifest.66babfa7ed42de0e2db7aa77b9d309e5.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "shopcoulsdon.org"});
