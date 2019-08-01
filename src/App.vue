77bc0b54f9549800a3d64bea08eb40870963bdf5
Kristian Matthews <kristian.matthews@me.com>
2019-08-01 20:41:38 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,10 @@
         <li class="nav-item">
           <router-link to="/map" class="nav-link" active-class="active">Map</router-link>
         </li>
+
+        <li class="nav-item">
+          <router-link to="/shops" class="nav-link" active-class="active">Shops</router-link>
+        </li>
       </ul>
     </div>
 
